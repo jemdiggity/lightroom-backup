e5af7ddc3c0f8c0d64c7377d4727f18204c261e3
Jeremy Hale <jeremy.hale@gmail.com>
2019-12-13 11:40:02 +0100

    Add function to restore files from glacier

--- sync.sh
+++ sync.sh
@@ -86,11 +86,18 @@ function check_options {
     echo "_dst empty"
     return $FAILURE
   fi
-  
+
   #Remove trailing slash for non-weird behaviour with sync program
   _src=${_src%/}
   _dst=${_dst%/}
   return $SUCCESS
+}
+
+function check_options_restore {
+  if [[ "$_bucket_name" == "" ]]; then
+    echo "_bucket_name empty"
+    return $FAILURE
+  fi
 }
 
 function init {
@@ -126,6 +133,53 @@ function push() {
   else
     echo "Pushing to $_dst"
     rsync -avz "$_src" "$_dst"
+  fi
+}
+
+function do_restore() {
+  #pipe into shell so failures are ignored
+  #for example, if the object is already being retrieved
+  aws s3api list-objects-v2 \
+    --bucket $_bucket_name \
+    --query "Contents[?StorageClass=='GLACIER']" \
+    --output text --prefix ${prefix} \
+    | awk -F $'\t' '{print $2}' \
+    | tr '\n' '\0' \
+    | xargs -L 1 -0 echo aws s3api restore-object --restore-request Days=7 --bucket "$_bucket_name" --key \
+    | sh
+}
+
+function restore() {
+  #restore from glacier
+  check_options_restore || return $FAILURE
+
+  date=$_arg_date
+
+  date_check "$date" || return $FAILURE
+
+  year=`echo $CLEAN_DATE | awk -F'-' '{print $1}'`
+  month=`echo $CLEAN_DATE | awk -F'-' '{print $2}'`
+  dayOfMonth=`echo $CLEAN_DATE | awk -F'-' '{print $3}'`
+
+   if [ "$dayOfMonth" == "" ]; then
+    if [ "$month" == "" ]; then
+      if [ "$year" == "" ]; then
+        echo "Include the date to restore"
+        die
+      else
+        echo "Restoring whole year: $year"
+        prefix="$year"
+        do_restore
+      fi
+    else
+      echo "Restoring whole month: $year-$month"
+      prefix="$year/$year-$month"
+      do_restore
+    fi
+  else
+    echo "Restoring date: $year-$month-$dayOfMonth"
+    prefix="$year/$year-$month-$dayOfMonth"
+    do_restore
   fi
 }
 
@@ -244,6 +298,43 @@ case "$subcommand" in
       eval "${_positional_names[ii]}=\${_positionals[ii]}" || die "Error during argument parsing, possibly an Argbash bug." 1
     done
     push
+    ;;
+  restore)
+    while test $# -gt 0; do
+      _key="$1"
+      case "$_key" in
+        -h|--help )
+          printf 'Usage: %s restore [-h|--help] [-v|--verbose] [-d|--date <arg>] <bucket>\n' "$0"
+          printf "\tExample: %s\n" "$0 restore -d 2014-07-13 \"lightroom-bucket\""
+          exit 1
+          ;;
+        -v|--verbose)
+          VERBOSE=1
+          ;;
+        -d|--date|--date=*)
+          _val="${_key##--option=}"
+          if test "$_val" = "$_key"
+          then
+            test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
+            _val="$2"
+            shift
+          fi
+          _arg_date="$_val"
+          ;;
+        *)
+          _positionals+=("$1")
+          ;;
+      esac
+      shift
+    done
+    _positional_names=('_bucket_name')
+    test ${#_positionals[@]} -lt 1 && _PRINT_HELP=yes die "FATAL ERROR: Not enough positional arguments - we require exactly 2, but got only ${#_positionals[@]}." 1
+    test ${#_positionals[@]} -gt 1 && _PRINT_HELP=yes die "FATAL ERROR: There were spurious positional arguments --- we expect exactly 2, but got ${#_positionals[@]} (the last one was: '${_positionals[*]: -1}')." 1
+    for (( ii = 0; ii < ${#_positionals[@]}; ii++))
+    do
+      eval "${_positional_names[ii]}=\${_positionals[ii]}" || die "Error during argument parsing, possibly an Argbash bug." 1
+    done
+    restore
     ;;
   pull)
     while test $# -gt 0; do
